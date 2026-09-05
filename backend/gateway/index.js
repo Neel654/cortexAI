9a5804ba5d88507553e64df58e92aba240492407
Neel654 <neelp61106@gmail.com>
2026-09-05 14:26:25 -0400

    Fix getMesg scope error, restore return in getMessages, add gateway request logging

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -8,6 +8,8 @@ import cookieParser from "cookie-parser"
 import protect from "./middleware/auth.middleware.js"
 import { getCurrentUser } from "./controllers/user.controller.js"
 import { proxyWithHeader } from "./utils/proxyWithHeader.js"
+import morgan from "morgan"
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,7 +20,7 @@ app.use(cors({
 }))
 
 app.use(cookieParser())
-
+app.use(morgan("dev"))
 app.use("/api/auth",proxy(process.env.AUTH_SERVICE))
 app.use("/api/chat",protect,proxyWithHeader(process.env.CHAT_SERVICE))
 app.use("/api/agent",protect,proxy(process.env.AGENT_SERVICE))
--- frontend/vite-project/src/features/getMessages.js
+++ frontend/vite-project/src/features/getMessages.js
@@ -3,6 +3,7 @@ import api from '../utils/axious'
 async function getMessages(id) {
   try {
     const {data}=await api.get(`/api/chat/get-messages/${id}`)
+    console.log(data)
     return data
   } catch (error) {
     console.log(error)
